9ab848d696aeb6a7324c1506136d1da2c7827639
Matias <matiasbenonip@gmail.com>
2024-03-27 13:17:49 -0300

    cambios enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TypingGameComponent } from './components/typing-game/typing-game.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: 'typing-game', component: TypingGameComponent }, // Define la ruta para TypingGameComponent
+  { path: 'typing-game', component: TypingGameComponent },
+  { path: '', component: HomeComponent }, // Define la ruta para TypingGameComponent
 ];
 
 @NgModule({
