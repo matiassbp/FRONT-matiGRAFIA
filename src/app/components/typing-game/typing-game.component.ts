d3aa8cfca90bdf95a5940bcf05617ebd8c841834
Matias <matiasbenonip@gmail.com>
2024-03-27 12:54:58 -0300

    Se agrega boton para borrar elementos de la lista. Ademas se realizan cambios esteticos.

--- src/app/components/typing-game/typing-game.component.ts
+++ src/app/components/typing-game/typing-game.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-typing-game',
   templateUrl: './typing-game.component.html',
-  styleUrls: ['./typing-game.component.css']
+  styleUrls: ['./typing-game.component.css'],
 })
 export class TypingGameComponent implements OnInit {
   exercises: any[] = [];
@@ -22,21 +22,34 @@ export class TypingGameComponent implements OnInit {
   }
 
   getExercisesFromDatabase() {
-    this.http.get<any[]>('http://localhost:3000/exercises')
-      .subscribe(
-        response => {
-          this.exercises = response;
-        },
-        error => {
-          console.error('Error al obtener los textos:', error);
-          alert('Ha ocurrido un error al intentar obtener los textos.');
-        }
-      );
+    this.http.get<any[]>('http://localhost:3000/exercises').subscribe(
+      (response) => {
+        this.exercises = response;
+      },
+      (error) => {
+        console.error('Error al obtener los textos:', error);
+        alert('Ha ocurrido un error al intentar obtener los textos.');
+      }
+    );
   }
 
   selectText(text: string) {
     this.currentSentence = text;
     this.gameStarted = true;
+  }
+
+  deleteText(exerciseId: string) {
+    this.http.delete<any>(`http://localhost:3000/exercises/${exerciseId}`).subscribe(
+      (response) => {
+        console.log(response.message);
+        alert('El texto ha sido eliminado exitosamente.');
+        this.getExercisesFromDatabase(); // Actualizar la lista de textos después de eliminar uno
+      },
+      (error) => {
+        console.error('Error al eliminar el texto:', error);
+        alert('Ha ocurrido un error al intentar eliminar el texto.');
+      }
+    );
   }
 
   startGame() {
@@ -62,18 +75,17 @@ export class TypingGameComponent implements OnInit {
   }
 
   saveTextToDatabase(text: string) {
-    this.http.post<any>('http://localhost:3000/save-text', { text })
-      .subscribe(
-        response => {
-          console.log(response.message);
-          alert('El texto ha sido guardado exitosamente.');
-          this.getExercisesFromDatabase(); // Actualizar la lista de textos después de guardar uno nuevo
-        },
-        error => {
-          console.error('Error al guardar el texto:', error);
-          alert('Ha ocurrido un error al intentar guardar el texto.');
-        }
-      );
+    this.http.post<any>('http://localhost:3000/save-text', { text }).subscribe(
+      (response) => {
+        console.log(response.message);
+        alert('El texto ha sido guardado exitosamente.');
+        this.getExercisesFromDatabase(); // Actualizar la lista de textos después de guardar uno nuevo
+      },
+      (error) => {
+        console.error('Error al guardar el texto:', error);
+        alert('Ha ocurrido un error al intentar guardar el texto.');
+      }
+    );
   }
 
   getTextColor(expectedChar: string, actualChar: string): string {
